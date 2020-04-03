c7cd6985504f79fa7c99cf4388a91fe564fc1685
github username <github email address>
2020-04-02 22:03:38 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,38 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '', 
+      password: ''
+    };
   }
 
+  handleUser = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  handleLogin = event => {
+    event.preventDefault()
+    let formData = {username: this.state.username, password: this.state.password}
+    if (this.state.username && this.state.password) {
+      this.props.handleLogin(formData)
+    }
+  }
+  
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleLogin} >
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleUser}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleUser}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: '', 
+      count: 280
+    };
+  }
+
+  captureTweet = (event) => {
+    if (this.state.count > 0){
+      this.setState({
+        tweet: event.target.value,
+        count: this.state.count - 1
+      })
+    }
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.tweet}onChange={this.captureTweet}/>{this.state.count}
       </div>
     );
   }
